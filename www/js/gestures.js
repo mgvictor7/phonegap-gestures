df8e89873e9c42e1c3cccd7e04d108d478259a13
victor <mgvictor7@gmail.com>
2017-01-15 13:10:06 +0000

    Añadida una zona para donde activaran los gestos usando Hammer.js

--- www/js/gestures.js
+++ www/js/gestures.js
@@ -1,15 +1,34 @@
-class App {
+class Gestures {
   init() {
-    const lightButton = document.querySelector('#lightBtn');
-    const darkButton = document.querySelector('#darkBtn');
-    lightButton.addEventListener('click', () => {
-      this.changeBodyColor('light');
-    }, false);
-
-    darkButton.addEventListener('click', () => {
-      this.changeBodyColor('dark');
-    }, false);
+    this.initButtons();
+    this.initFastClick();
+    this.initHammer();
   }
+
+  initButtons() {
+    const lightButton = document.getElementById('lightBtn');
+    const darkButton = document.getElementById('darkBtn');
+    lightButton.addEventListener('click', () => { this.changeBodyColor('light'); });
+    darkButton.addEventListener('click', () => { this.changeBodyColor('dark'); });
+  }
+
+  initFastClick() {
+    FastClick.attach(document.body);
+  }
+
+  initHammer() {
+    const gesturesZone = document.getElementById('gestures-zone');
+    const hammer = new Hammer(gesturesZone);
+
+    // Iniciar eventos que el Hammer no inicia por defecto
+    hammer.get('pinch').set({ enable: true });
+    hammer.get('rotate').set({ enable: true });
+
+    hammer.on('tap doubletap swipe press rotate', (e) => {
+      document.getElementById('info').innerHTML = e.type;
+    });
+  }
+
 
   changeBodyColor(classColor) {
     document.body.className = classColor;
@@ -18,8 +37,7 @@ class App {
 
 if ('addEventListener' in document) {
   document.addEventListener('DOMContentLoaded', () => {
-    FastClick.attach(document.body);
-    const app = new App();
+    const app = new Gestures();
     app.init();
   });
 }
